125594d8d3fcc5aff4b039b7fd812f0d900e7981
Patrick Maloney <boyscoutpatrick813@gmail.com>
2023-05-01 01:34:22 -0500

    Adjust to work in things

---
+++ bot.py
@@ -0,0 +1,34 @@
+import guilded
+from guilded.ext import commands
+from dotenv import load_dotenv
+import radar_cog
+import spc_cog
+import swpc_cog
+import os
+
+description = 'A bot to get information from NOAA/NWS'
+
+bot = commands.Bot(command_prefix=["!nws ", "!noaa "], description=description)
+
+
+@bot.event
+async def on_ready():
+    print(f'Logged in as {bot.user} (ID: {bot.user.id})')
+    print('------')
+    print('Loading Cogs')
+    bot.add_cog(spc_cog.SPC(bot))
+    print('Loaded SPC cog')
+    bot.add_cog(swpc_cog.SWPC(bot))
+    print('Loaded SWPC cog')
+    bot.add_cog(radar_cog.Radar_Cog(bot))
+    print('Loaded Radar cog')
+    print('Cog loading complete')
+    print('------')
+
+
+@bot.command(name="ping")
+async def ping(ctx):
+    await ctx.send('Pong.')
+
+load_dotenv()
+bot.run(os.environ['TOKEN'])
--- main.py
+++ main.py
@@ -1,34 +1,5 @@
-import guilded
-from guilded.ext import commands
-from dotenv import load_dotenv
-import radar_cog
-import spc_cog
-import swpc_cog
-import os
+import subprocess
 
-description = 'A bot to get information from NOAA/NWS'
+subprocess.run("pip3 install -U --prefix .local -r requirements.txt", shell=True)
 
-bot = commands.Bot(command_prefix=["!nws ", "!noaa "], description=description)
-
-
-@bot.event
-async def on_ready():
-    print(f'Logged in as {bot.user} (ID: {bot.user.id})')
-    print('------')
-    print('Loading Cogs')
-    bot.add_cog(spc_cog.SPC(bot))
-    print('Loaded SPC cog')
-    bot.add_cog(swpc_cog.SWPC(bot))
-    print('Loaded SWPC cog')
-    bot.add_cog(radar_cog.Radar_Cog(bot))
-    print('Loaded Radar cog')
-    print('Cog loading complete')
-    print('------')
-
-
-@bot.command(name="ping")
-async def ping(ctx):
-    await ctx.send('Pong.')
-
-load_dotenv()
-bot.run(os.environ['TOKEN'])
+subprocess.run("python3 /home/container/bot.py")
